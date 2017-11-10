e199f042e1f9e06b9287b76f9225b2d57e9b59de
Rory Comiskey <rorycomiskey@hotmail.com>
2017-11-10 16:41:16 +0000

    add priority field

--- todoitem/forms.py
+++ todoitem/forms.py
@@ -4,4 +4,4 @@ from .models import TodoItem
 class TodoItemForm(forms.ModelForm):
    class Meta:
        model = TodoItem
-       fields = ('name', 'done')
+       fields = ('name', 'priority', 'done')
