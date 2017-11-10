7e77bcc7116b6e1ba7d7592b5ae1c1d6cb5a0d9d
Rory Comiskey <rorycomiskey@hotmail.com>
2017-11-10 12:54:16 +0000

    third commit

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['todo-rcomiskey.c9users.io']
+ALLOWED_HOSTS = ['todo-rcomiskey.c9users.io', 'rcomiskey-todo.herokuapp.com']
 
 
 # Application definition
