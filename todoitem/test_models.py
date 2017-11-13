237088eedc02b915962f2dfb25fd5797d3be623f
Rory Comiskey <rorycomiskey@hotmail.com>
2017-11-13 15:08:09 +0000

    Passed tests

--- todoitem/test_models.py
+++ todoitem/test_models.py
@@ -17,8 +17,8 @@ class TestItemModel(TestCase):
         
     def test_not_done_item_as_string(self):
         item = TodoItem(name="An Item", done=False)
-        self.assertEqual("An Item(N)", str(item))
+        self.assertEqual("An Item (N)", str(item))
         
     def test_item_as_string(self):
         item = TodoItem(name="An Item", done=True)
-        self.assertEqual("An Item(D)", str(item))
+        self.assertEqual("An Item (D)", str(item))
